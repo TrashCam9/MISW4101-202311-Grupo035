b6e2cd3ef55ee832c10bba0ef0c0c5307b4a3e67
Cristóbal Arroyo <j.arroyo@uniandes.edu.co>
2023-03-03 11:55:55 -0500

    GeneracionDatosaleatorios (#52)
    * Añadir base de datos a .gitignore
    
    Signed-off-by: Crola1702 <j.arroyo@uniandes.edu.co>
    
    * Automatizar prueba de crear clave
    
    ---------
    
    Signed-off-by: Crola1702 <j.arroyo@uniandes.edu.co>

--- tests/test_clave.py
+++ tests/test_clave.py
@@ -1,5 +1,7 @@
 import unittest
 
+from faker import Faker
+
 from src.logica.FachadaCajaDeSeguridad import FachadaCajaDeSeguridad
 from src.modelo.clave import Clave
 from src.modelo.declarative_base import Session
@@ -9,13 +11,19 @@ class ClaveTestCase(unittest.TestCase):
     def setUp(self) -> None:
         self.fachada = FachadaCajaDeSeguridad()
         self.session = Session()
+        self.data_factory = Faker()
 
     def test_crear_clave_errada(self):
         self.assertRaises(ValueError, self.fachada.crear_clave, 123, 123, 123)
 
     def test_crear_clave(self):
-        self.fachada.crear_clave('Clave bonita', 'HelloFromTheOtherSide', 'Lyrics de Hello - Adele')
-        clave = self.session.query(Clave).filter(Clave.nombre == 'Clave bonita').first()
+        nombre_clave = self.data_factory.word()
+        self.fachada.crear_clave(
+            nombre=nombre_clave,
+            clave=self.data_factory.password(),
+            pista=self.data_factory.sentence())
+        
+        clave = self.session.query(Clave).filter(Clave.nombre == nombre_clave).first()
         self.assertNotEqual(clave, None)
 
     def test_generar_clave(self):
