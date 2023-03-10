c771e6bfe0c183b4350c4d7d0541b3d6949e246f
Crola1702 <j.arroyo@uniandes.edu.co>
2023-03-10 16:41:26 -0500

    Test: llaves correctas (Rojo)
    Signed-off-by: Crola1702 <j.arroyo@uniandes.edu.co>

--- tests/test_reporteDeSeguridad.py
+++ tests/test_reporteDeSeguridad.py
@@ -12,5 +12,13 @@ class ReporteDeSeguridadTestCase(unittest.TestCase):
         reporte = self.fachada.dar_reporte_seguridad()
         self.assertIsInstance(reporte, dict)
 
+    def test_llaves_correctas(self):
+        # Debe tener las llaves: logins, ids, tarjetas, secretos, inseguras, avencer, masdeuna, nivel
+        llaves = ['logins', 'ids', 'tarjetas', 'secretos', 'inseguras', 'avencer', 'masdeuna', 'nivel']
+        reporte = self.fachada.dar_reporte_seguridad()
+        for llave in llaves:
+            self.assertIn(llave, reporte.keys())
+
+
     def tearDown(self) -> None:
         return super().tearDown()
