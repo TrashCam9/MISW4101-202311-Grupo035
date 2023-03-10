ba761e7f95ca6644b1b9ba6de9e761741e9b469f
Crola1702 <j.arroyo@uniandes.edu.co>
2023-03-10 17:31:35 -0500

    Test: numero de identificaciones (Rojo)
    Signed-off-by: Crola1702 <j.arroyo@uniandes.edu.co>

--- tests/test_reporteDeSeguridad.py
+++ tests/test_reporteDeSeguridad.py
@@ -2,7 +2,7 @@ import unittest
 
 from src.modelo.declarative_base import session
 
-from src.modelo.elemento import Login
+from src.modelo.elemento import Login, Identificacion
 from src.modelo.clave import Clave
 
 from src.logica.FachadaCajaDeSeguridad import FachadaCajaDeSeguridad
@@ -11,13 +11,29 @@ from tests import testing_utils
 
 class ReporteDeSeguridadTestCase(unittest.TestCase):
     listaLogins: "list[Clave]" = []
+    listaIds: "list[Identificacion]" = []
 
     def setUp(self) -> None:
         self.fachada = FachadaCajaDeSeguridad()    
         self.session = session   
-        self.data_factory = testing_utils.data_factory                 
+        self.data_factory = testing_utils.data_factory
+        self.clave: Clave
+
+        if session.query(Clave).count == 0:
+            self.clave = testing_utils.crear_clave()
+        else:
+            self.clave = session.query(Clave).first()
+
+        if session.query(Login).count() == 0:
+            testing_utils.crear_5_logins_aleatorios(self.clave)
+        self.listaLogins = session.query(Login).all()
+
+        if session.query(Identificacion).count() == 0:
+            testing_utils.crear_5_identificacioens_aleatorias()
+        self.listaIds = session.query(Identificacion).all()
 
         return super().setUp()
+
     
     def test_retorna_diccionario(self):
         reporte = self.fachada.dar_reporte_seguridad()
@@ -31,11 +47,15 @@ class ReporteDeSeguridadTestCase(unittest.TestCase):
 
     def test_num_logins(self):
         listaLogins = session.query(Login).all()
-
         reporte = self.fachada.dar_reporte_seguridad()
         self.assertIsInstance(reporte['logins'], int)
         self.assertEqual(reporte['logins'], len(listaLogins))
 
+    def test_num_ids(self):
+        listaIds = session.query(Identificacion).all()
+        reporte = self.fachada.dar_reporte_seguridad()
+        self.assertIsInstance(reporte['ids'], int)
+        self.assertEqual(reporte['ids'], len(listaIds))
 
     def tearDown(self) -> None:
         return super().tearDown()
--- tests/testing_utils.py
+++ tests/testing_utils.py
@@ -2,7 +2,9 @@ from faker import Faker
 
 from src.modelo.declarative_base import session
 from src.modelo.clave import Clave
-from src.modelo.elemento import Elemento, ElementoConClave
+from src.modelo.elemento import Login, Identificacion
+
+from datetime import date
 
 data_factory = Faker()
 
@@ -19,3 +21,32 @@ def crear_clave():
     session.add(clave)
     session.commit()
     return clave
+
+
+def crear_5_logins_aleatorios(clave: Clave):
+    for _ in range(5):
+        login = Login(tipo = "login",
+                        nombre = data_factory.word(),
+                        nota = data_factory.sentence(),
+                        clave = clave.id,
+                        usuario = data_factory.user_name(),
+                        email = data_factory.email(),
+                        url = data_factory.url())
+        session.add(login)
+    session.commit()
+
+def crear_5_identificacioens_aleatorias():
+    for _ in range(5):
+        fecha_nacimiento = date(*map(int, data_factory.date().split("-")))
+        fecha_expedicion = fecha_nacimiento.replace(year=fecha_nacimiento.year + 18)
+        fecha_vencimiento = fecha_expedicion.replace(year=fecha_expedicion.year + 10)
+        identificacion = Identificacion(tipo = "identificacion",
+                                        nombre = data_factory.word(),
+                                        nota = data_factory.sentence(),
+                                        numero = data_factory.random_int(),
+                                        nombreCompleto = data_factory.name(),
+                                        fechaNacimiento = fecha_nacimiento,
+                                        fechaExpedicion = fecha_expedicion,
+                                        fechaVencimiento = fecha_vencimiento)
+        session.add(identificacion)
+    session.commit()
