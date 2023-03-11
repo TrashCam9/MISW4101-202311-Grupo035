bc4f5254836ef9e3a7fcc460a358098d8669d924
Cristóbal Arroyo <j.arroyo@uniandes.edu.co>
2023-03-11 14:04:24 +0000

    Just saving changes before I go
    Signed-off-by: Cristóbal Arroyo <j.arroyo@uniandes.edu.co>

--- tests/testing_utils.py
+++ tests/testing_utils.py
@@ -8,6 +8,7 @@ from datetime import date
 
 data_factory = Faker()
 
+
 def give_unique_word():
     word = data_factory.unique.word()
     while session.query(Clave).filter(Clave.nombre == word).count() > 0:
@@ -25,54 +26,66 @@ def crear_clave():
 
 def crear_5_logins_aleatorios(clave: Clave):
     for _ in range(5):
-        login = Login(tipo = "login",
-                        nombre = data_factory.word(),
-                        nota = data_factory.sentence(),
-                        clave = clave.id,
-                        usuario = data_factory.user_name(),
-                        email = data_factory.email(),
-                        url = data_factory.url())
+        login = Login(tipo="login",
+                      nombre=data_factory.word(),
+                      nota=data_factory.sentence(),
+                      clave=clave.id,
+                      usuario=data_factory.user_name(),
+                      email=data_factory.email(),
+                      url=data_factory.url())
         session.add(login)
     session.commit()
+
 
 def crear_5_identificacioens_aleatorias():
     for _ in range(5):
         fecha_nacimiento = date(*map(int, data_factory.date().split("-")))
-        fecha_expedicion = fecha_nacimiento.replace(year=fecha_nacimiento.year + 18)
-        fecha_vencimiento = fecha_expedicion.replace(year=fecha_expedicion.year + 10)
-        identificacion = Identificacion(tipo = "identificacion",
-                                        nombre = data_factory.word(),
-                                        nota = data_factory.sentence(),
-                                        numero = data_factory.random_int(),
-                                        nombreCompleto = data_factory.name(),
-                                        fechaNacimiento = fecha_nacimiento,
-                                        fechaExpedicion = fecha_expedicion,
-                                        fechaVencimiento = fecha_vencimiento)
+        fecha_expedicion = fecha_nacimiento.replace(
+            year=fecha_nacimiento.year + 18)
+        fecha_vencimiento = fecha_expedicion.replace(
+            year=fecha_expedicion.year + 10)
+        identificacion = Identificacion(tipo="identificacion",
+                                        nombre=data_factory.word(),
+                                        nota=data_factory.sentence(),
+                                        numero=data_factory.random_int(),
+                                        nombreCompleto=data_factory.name(),
+                                        fechaNacimiento=fecha_nacimiento,
+                                        fechaExpedicion=fecha_expedicion,
+                                        fechaVencimiento=fecha_vencimiento)
         session.add(identificacion)
     session.commit()
+
 
 def crear_5_tarjetas_aleatorias(clave: Clave):
     for _ in range(5):
         fecha_vencimiento = date(*map(int, data_factory.date().split("-")))
-        tarjeta = Tarjeta(tipo = "tarjeta",
-                          nombre = data_factory.word(),
-                          nota = data_factory.sentence(),
-                          clave = clave.id,
-                          numero = data_factory.random_int(),
-                          titular = data_factory.name(),
-                          fecha_vencimiento = fecha_vencimiento,
-                          codigo_seguridad = data_factory.random_int(),
-                          direccion = data_factory.address(),
-                          telefono = data_factory.phone_number())
+        tarjeta = Tarjeta(tipo="tarjeta",
+                          nombre=data_factory.word(),
+                          nota=data_factory.sentence(),
+                          clave=clave.id,
+                          numero=data_factory.random_int(),
+                          titular=data_factory.name(),
+                          fecha_vencimiento=fecha_vencimiento,
+                          codigo_seguridad=data_factory.random_int(),
+                          direccion=data_factory.address(),
+                          telefono=data_factory.phone_number())
         session.add(tarjeta)
     session.commit()
 
+
 def crear_5_secretos_aleatorios(clave: Clave):
     for _ in range(5):
-        secreto = Secreto(tipo = "secreto",
-                          nombre = data_factory.word(),
-                          nota = data_factory.sentence(),
-                          clave = clave.id,
-                          secreto = data_factory.sentence())
+        secreto = Secreto(tipo="secreto",
+                          nombre=data_factory.word(),
+                          nota=data_factory.sentence(),
+                          clave=clave.id,
+                          secreto=data_factory.sentence())
         session.add(secreto)
     session.commit()
+
+
+
+def verificar_clave_segura(clave: str) -> bool:
+    return len(clave) >= 8 and ' ' not in clave and any(char.isdigit() for char in clave) and any(char.isupper() for char in clave) and any(char.islower() for char in clave) and any(char in "?-*!@#$/(){}=.,;:" for char in clave)
+
+print(verificar_clave_segura(crear_clave()))
